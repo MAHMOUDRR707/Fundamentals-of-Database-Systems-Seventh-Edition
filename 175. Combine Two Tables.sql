8541c04160f06517a59600e9283a92aaa30b288f
Mahmoud Reda <55753782+MAHMOUDRR707@users.noreply.github.com>
2022-04-02 15:41:01 +0200

    Delete 175. Combine Two Tables.sql

--- 175. Combine Two Tables.sql
+++
@@ -1,6 +0,0 @@
-#link:https://leetcode.com/problems/combine-two-tables/
-
-SELECT P.firstName  , P.lastName  , A.city , A.state
-FROM Person as P 
-LEFT JOIN Address  AS A
-ON P.personId = A.personId
